d60995af20b1c1ea43acfc7c6f3451770b6554b5
0xRoot <vicky03485356225@gmail.com>
2024-11-07 11:57:12 -0800

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='text-3xl text-red-500'>hi</div>
+    </>
+  )
+}
+
+export default App
